5305b47f9e0c6f4646bc4f1617857b2d951541f1
Mark <markh4b@hotmail.com>
2015-02-09 17:40:43 +0100

    Better icons and material

--- Arena.ocd/Environment.ocd/GameConfiguration.ocd/Script.c
+++ Arena.ocd/Environment.ocd/GameConfiguration.ocd/Script.c
@@ -14,12 +14,58 @@ protected func OnCloseMainMenu()
 	SelectionMajor();
 }
 
+protected func OnOpenMainMenu()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuChooseGoal()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureBots()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureGoal()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureItems()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureItemsCustom()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureItemSlot()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureRules()
+{
+	SelectionMinor();
+}
+
+protected func OnMenuConfigureTeams()
+{
+	SelectionMinor();
+}
+
+
 protected func SelectionMajor()
 {
-	Sound("SelectMajor");
+	Sound("SelectMajor", true);
 }
 
 protected func SelectionMinor()
 {
-	Sound("SelectMinor");
+	Sound("SelectMinor", true);
 }
