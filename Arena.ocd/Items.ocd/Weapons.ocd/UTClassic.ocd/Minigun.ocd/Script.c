766e346d8fb3df93cda4c839c5c3d697c5b1760f
Mark <markh4b@hotmail.com>
2015-04-13 06:22:43 +0200

    Added "minigun"

--- Arena.ocd/Items.ocd/Weapons.ocd/UTClassic.ocd/Minigun.ocd/Script.c
+++ Arena.ocd/Items.ocd/Weapons.ocd/UTClassic.ocd/Minigun.ocd/Script.c
@@ -28,35 +28,40 @@ public func GetAmmoContainer()
 	return Contained();
 }
 
+
 local fire_modes =
 {
 	default = 
 	{
-		name = 				"default",
-		icon = 				nil,
-		condition = 		nil,
+		name = 				"default", // string - menu caption
+		icon = 				nil, // id - menu icon
+		condition = 		nil, // string - callback for a condition
 		
 		ammo_id = 			Ammo_Pistol,
-		ammo_usage =		1,
-		ammo_rate =			1,
+		ammo_usage =		1,	// this many units of ammo
+		ammo_rate =			1, // per this many shots fired
 	
-		delay_prior = 		0,
-		delay_reload =		0,
-		delay_recover = 	15,
+		delay_prior = 		0, // time before the first shot is fired
+		delay_reload =		260, // time to reload, in frames
+		delay_recover = 	4, // time between consecutive shots
+		delay_burst = 		0, // time between consecutive bursts
 	
-		mode = 			 WEAPON_FM_Single,
+		mode = 			 WEAPON_FM_Auto,
 	
-		damage = 			15, 
+		damage = 			6, 
 		damage_type = 		nil,	
 	
 		projectile_id = 	Projectile_Bullet,
-		projectile_speed = 	180,
+		projectile_speed = 	210,
 		projectile_range = 600,
 		projectile_distance = 10,
 		projectile_offset_y = -6,
+		projectile_number = 1,
+		projectile_spread = 0, // default inaccuracy of a single projectile
+		projectile_spread_factor = 100, // factor
 
-		spread = 1,
-		spread_factor = 100,
+		spread = 2,			   // inaccuracy from prolonged firing
+		spread_factor = 100,   // factor
 	},
 	
 	secondary = 
@@ -70,21 +75,21 @@ local fire_modes =
 		ammo_rate =			1,
 	
 		delay_prior = 		0,
-		delay_reload =		0,
+		delay_reload =		260,
 		delay_recover = 	10,
 	
 		mode = 			 WEAPON_FM_Single,
 	
-		damage = 			15, 
+		damage = 			6, 
 		damage_type = 		nil,	
 	
 		projectile_id = 	Projectile_Bullet,
-		projectile_speed = 	180,
+		projectile_speed = 	210,
 		projectile_range = 600,
 		projectile_distance = 10,
 		projectile_offset_y = -6,
 
-		spread = 3,
+		spread = 6,
 		spread_factor = 100,
 	},
 };
