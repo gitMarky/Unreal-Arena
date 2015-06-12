adcef82da043ce795aef78db230c7c07ea02e73e
Mark <markh4b@hotmail.com>
2015-06-12 22:36:16 +0200

    More weapons
    Bio rifle, sniper rifle, redeemer warhead

---
+++ Arena.ocd/Items.ocd/Weapons.ocd/UTClassic.ocd/BioRifle.ocd/Script.c
@@ -0,0 +1,146 @@
+#include Library_Weapon
+#include Plugin_Weapon_FiremodeByUse
+
+local Name = "$Name$";
+local Description = "$Description$";
+local Collectible = 1;
+
+
+public func GetCarryMode(object user) {    if (is_selected) return CARRY_Hand; }
+public func GetCarrySpecial(object user) { if (is_selected) return "pos_hand2"; }
+public func GetCarryBone() { return "Base"; }
+public func GetCarryTransform()
+{
+	return Trans_Rotate(120, 0, 1, 0);
+}
+
+local animation_set = {
+		AimMode        = AIM_Position, // The aiming animation is done by adjusting the animation position to fit the angle
+		AnimationAim   = "CrossbowAimArms",
+};
+
+func Hit()
+{
+	Sound("GeneralHit?");
+}
+
+public func GetAmmoSource(id ammo)
+{
+	return AMMO_Source_Container;
+}
+
+public func GetAmmoContainer()
+{
+	return Contained();
+}
+
+local fire_modes =
+{
+	primary	= 
+	{
+		name = 				"primary",
+		icon = 				nil,
+		condition = 		nil,
+		
+		ammo_id = 			Ammo_Pistol,
+		ammo_usage =		1,
+		ammo_rate =			1,
+	
+		delay_prior = 		0,
+		delay_reload =		0,
+		delay_recover = 	15,
+	
+		mode = 			 WEAPON_FM_Single,
+	
+		damage = 			15, 
+		damage_type = 		nil,	
+	
+		projectile_id = 	Projectile_Bullet,
+		projectile_speed = 	180,
+		projectile_range = 600,
+		projectile_distance = 10,
+		projectile_offset_y = -6,
+		projectile_spread = [7, 4],
+
+		spread = [0, 1],
+	},
+	
+	secondary = 
+	{
+		name = 				"secondary",
+		icon = 				nil,
+		condition = 		nil,
+		
+		ammo_id = 			Ammo_Pistol,
+		ammo_usage =		1,
+		ammo_rate =			1,
+	
+		delay_prior = 		0,
+		delay_reload =		0,
+		delay_recover = 	10,
+	
+		mode = 			 WEAPON_FM_Single,
+	
+		damage = 			15, 
+		damage_type = 		nil,	
+	
+		projectile_id = 	Projectile_Bullet,
+		projectile_speed = 	180,
+		projectile_range = 600,
+		projectile_distance = 10,
+		projectile_offset_y = -6,
+		
+		projectile_spread = [4, 1],
+
+		spread = [0, 1], 
+	},
+};
+
+public func FireSound(object user, proplist firemode)
+{
+	Sound("enforcer-fire", nil, nil, nil, nil, true);
+}
+
+public func OnFireProjectile(object user, object projectile, proplist firemode)
+{
+	projectile->Trail(2, 80);
+}
+
+public func FireEffect(object user, int angle, proplist firemode)
+{
+	// this does nothing at the moment, 
+	// probably because the animation is too small to be noticeable on the attached mesh :(
+	
+	//PlayAnimation("Fire", 1, Anim_Linear(0, 0, GetAnimationLength("Fire"), 10, ANIM_Remove), Anim_Const(1000));
+	//PlayAnimation("Fire", 6, Anim_Linear(0, 0, GetAnimationLength("Fire"), 10, ANIM_Hold), Anim_Const(1000));
+	//PlayAnimation("Fire", 6, Anim_Linear(0, 0, GetAnimationLength("Fire"), animation_set["ShootTime"], ANIM_Hold), Anim_Const(1000));
+
+	//SetAction("Fire");
+	
+	// muzzle flash
+	
+	var x = +Sin(angle, firemode.projectile_distance);
+	var y = -Cos(angle, firemode.projectile_distance) + firemode.projectile_offset_y;
+	 
+	
+	EffectMuzzleFlash(user, x, y, angle, 10, false, true);
+		
+	// casing
+	
+	x = +Sin(angle, firemode.projectile_distance / 2);
+	y = -Cos(angle, firemode.projectile_distance / 2) + firemode.projectile_offset_y;
+
+	CreateCartridgeEffect("Cartridge_Pistol", 2, x, y, user->GetXDir() + Sin(-angle, 10), user->GetYDir() - RandomX(12, 15));
+}
+
+local ActMap = {
+	Fire = {
+		Prototype = Action,
+		Name = "Fire",
+		Procedure = DFA_NONE,
+		Length = 10,
+		Delay = 1,
+		NextAction = "Idle",
+		Animation = "Fire",
+	},
+};
---
+++ Arena.ocd/Items.ocd/Weapons.ocd/UTClassic.ocd/Sniper.ocd/Script.c
@@ -0,0 +1,165 @@
+#include Library_Weapon
+#include Plugin_Weapon_FiremodeByUse
+
+local Name = "$Name$";
+local Description = "$Description$";
+local Collectible = 1;
+
+
+public func GetCarryMode(object user) {    if (is_selected) return CARRY_Hand; }
+public func GetCarrySpecial(object user) { if (is_selected) return "pos_hand2"; }
+public func GetCarryBone() { return "Base"; }
+public func GetCarryTransform()
+{
+	return Trans_Rotate(-60, 0, 1, 0);
+}
+
+local animation_set = {
+		AimMode        = AIM_Position, // The aiming animation is done by adjusting the animation position to fit the angle
+		AnimationAim   = "CrossbowAimArms",
+};
+	
+func Hit()
+{
+	Sound("GeneralHit?");
+}
+
+public func GetAmmoSource(id ammo)
+{
+	return AMMO_Source_Container;
+}
+
+public func GetAmmoContainer()
+{
+	return Contained();
+}
+
+
+local fire_modes =
+{
+	primary = 
+	{
+		name = 				"primary", // string - menu caption
+		icon = 				nil, // id - menu icon
+		condition = 		nil, // string - callback for a condition
+		
+		ammo_id = 			Ammo_Pistol,
+		ammo_usage =		1,	// this many units of ammo
+		ammo_rate =			1, // per this many shots fired
+	
+		delay_charge =      0,
+		delay_recover = 	4, // time between consecutive shots
+		delay_cooldown = 	20,
+		delay_reload =		260, // time to reload, in frames
+	
+		mode = 			 WEAPON_FM_Auto,
+	
+		damage = 			6, 
+		damage_type = 		nil,	
+	
+		projectile_id = 	Projectile_Bullet,
+		projectile_speed = 	210,
+		projectile_range = 600,
+		projectile_distance = 18,
+		projectile_offset_y = -2, // -4
+		projectile_number = 1,
+		projectile_spread = [5, 2], // 2 - default inaccuracy of a single projectile
+
+		spread = [0, 1],			   // inaccuracy from prolonged firing
+	},
+	
+	secondary = 
+	{
+		name = 				"secondary",
+		icon = 				nil, // id - menu icon
+		condition = 		nil, // string - callback for a condition
+		
+		ammo_id = 			Ammo_Pistol,
+		ammo_usage =		1,	// this many units of ammo
+		ammo_rate =			1, // per this many shots fired
+	
+		delay_charge =      0,
+		delay_recover = 	2, // time between consecutive shots
+		delay_cooldown = 	20,
+		delay_reload =		260, // time to reload, in frames
+	
+		mode = 			 WEAPON_FM_Auto,
+	
+		damage = 			6, 
+		damage_type = 		nil,	
+	
+		projectile_id = 	Projectile_Bullet,
+		projectile_speed = 	210,
+		projectile_range = 600,
+		projectile_distance = 18,
+		projectile_offset_y = -2, // -4
+		projectile_number = 1,
+		projectile_spread = [15, 2], // 6 - default inaccuracy of a single projectile
+
+		spread = [0, 1],			   // inaccuracy from prolonged firing	},
+	},
+};
+
+local weapon_properties = 
+{
+		gfx_distance = 16,
+		gfx_offset_y = -2,
+};
+
+public func FireSound(object user, proplist firemode)
+{
+	if (firemode.name == fire_modes.primary.name)
+	{
+		Sound("m1-fire-reg", nil, nil, nil, 1);
+	}
+	else
+	{
+		Sound("m1-fire-alt", nil, nil, nil, 1);
+	}
+}
+
+
+public func OnFireProjectile(object user, object projectile, proplist firemode)
+{
+	projectile->Trail(1, 60);
+}
+
+public func OnStartCooldown(object user, proplist firemode)
+{
+	
+	Sound("m1-fire-reg", nil, nil, nil, -1);
+	Sound("m1-fire-alt", nil, nil, nil, -1);
+	Sound("m1-cooldown");
+}
+
+public func FireEffect(object user, int angle, proplist firemode)
+{
+	
+	// muzzle flash
+	
+	var x = +Sin(angle, firemode.projectile_distance);
+	var y = -Cos(angle, firemode.projectile_distance) + firemode.projectile_offset_y;
+
+	EffectMuzzleFlash(user, x, y, angle, RandomX(15, 25), false, true);
+		
+	// casing
+	
+	x = +Sin(angle, firemode.projectile_distance / 2);
+	y = -Cos(angle, firemode.projectile_distance / 2) + firemode.projectile_offset_y;
+
+	var dir = (user->GetDir() * 2) - 1;
+
+	CreateCartridgeEffect("Cartridge_Pistol", 2, x, y, - dir * Cos(angle - 35 * dir, RandomX(30, 45)), - dir * Sin(angle - 35 * dir, RandomX(30, 45)));
+}
+
+local ActMap = {
+	Fire = {
+		Prototype = Action,
+		Name = "Fire",
+		Procedure = DFA_NONE,
+		Length = 10,
+		Delay = 1,
+		NextAction = "Idle",
+		Animation = "Fire",
+	},
+};
