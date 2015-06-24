8fa6f69006be12311c905ad37f5a9ff2da9fed18
Mark <markh4b@hotmail.com>
2015-06-24 19:49:49 +0200

    Sniper rifle effect

--- Arena.ocd/Items.ocd/Weapons.ocd/UTClassic.ocd/Sniper.ocd/Shot.ocd/Script.c
+++ Arena.ocd/Items.ocd/Weapons.ocd/UTClassic.ocd/Sniper.ocd/Shot.ocd/Script.c
@@ -5,17 +5,24 @@ protected func Initialize()
 	SetObjectBlitMode(GFX_BLIT_Additive);
 }
 
-private func OnLaunch()
-{
-}
-
-private func OnLaunched()
-{
-}
-
 public func OnHitLandscape()
 {
 		Sound("BulletHitGround?");
 		CreateImpactEffect(this.damage / 3);
 }
 
+public func OnHitObject(object target)
+{
+		CreateImpactEffect(this.damage / 3);
+}
+
+public func OnHitScan(int x_start, int y_start, int x_end, int y_end)
+{
+	var laser = CreateObject(LaserEffect, x_start - GetX(), y_start - GetY(), NO_OWNER);
+	
+	laser->Line(x_start, y_start, x_end, y_end)
+		 ->SetWidth(1)
+		 ->SetLifetime(10)
+		 ->Color(RGBa(255, 200, 0))
+		 ->Activate();
+}
