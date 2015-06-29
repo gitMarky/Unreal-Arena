40f032b85064250a72deda52878b1b9328715428
Mark <markh4b@hotmail.com>
2015-06-29 06:16:57 +0200

    Disintegration effect
    Moved the effect to the arena games library. The clonk needs a corpse object now.

---
+++ System.ocg/Mod_ClonkMisc.c
@@ -0,0 +1,17 @@
+/**
+ Several small modifications to the clonk can be found here.
+ @version 0.1.0
+ */
+
+#appendto Clonk
+
+func Recruitment()
+{
+	// remove actions that are not intended
+	if(this.ActMap == this.Prototype.ActMap)
+		this.ActMap = {Prototype = this.Prototype.ActMap};
+	this.ActMap["Hangle"] = nil;
+	this.ActMap["Scale"] = nil;
+
+	return _inherited(...);
+}
