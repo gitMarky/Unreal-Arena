4c2ced584c23d339476cf6ec291e178ccbe7ffe7
Mark <markh4b@hotmail.com>
2015-07-09 22:40:31 +0200

    Improved SmokeStack
    Uses particles now instead of objects.

--- Arena.ocd/Effects.ocd/SmokeCustom.ocd/Script.c
+++ Arena.ocd/Effects.ocd/SmokeCustom.ocd/Script.c
@@ -1,15 +1,4 @@
 
-
-global func SmokeX(int _x, int _y, int con, int move_style, int time, int dwColor1, int dwColor2,int effect_style)
-{
-	return CreateObject(Effect_SmokeCustom, 0, 0, NO_OWNER)
-	       ->Activate(GetX() + _x, GetY() + _y, con / 2, move_style, time,
-	                  dwColor1, dwColor2, effect_style);
-}
-
-
-local movement,effect,max_con,max_time,r1,g1,b1,a1,r2,g2,b2,a2;
-
 /*
 movement
 
@@ -29,26 +18,12 @@ effect
 
 */
 
-func Activate(int _x, int _y, int con, int move_style, int time, int dwColor1, int dwColor2, int effect_style)
+global func SmokeX(int x, int y, int con, int move_style, int lifetime, int dwColor1, int dwColor2,int effect_style)
 {
-	SetAction("Smoke");
-	SetCon(75 * con / 100);
-	max_con = con;
-	
-	max_time = 32;
-	if (time)
-		max_time = time;
-	
-	if (max_time < 1)
-		max_time = 1;
-
-	movement = move_style;
-	effect = effect_style;
-	
-	SetPosition(_x, _y);
-	
-	SetDir(Random(4));
-	SetPhase(Random(4));
+	con /= 2;
+	var size = con * 64 / 100;
+	var r1, g1, b1, a1;
+	var r2, g2, b2, a2;
 	
 	r1 = GetRGBaValue(dwColor1, 1);
 	g1 = GetRGBaValue(dwColor1, 2);
@@ -65,123 +40,56 @@ func Activate(int _x, int _y, int con, int move_style, int time, int dwColor1, i
 		a1 = 200;
 		a2 = 0;
 	}
-	
-	if (!effect)
-		SetClrModulation(RGBa(255, 255, 255, 123));
-	
-	Move2Move();
-	AddEffect("IntMove", this, 1, 1, this);
-}
 
-public func FxIntMoveTimer()
-{
-	Move2Move();
-}
+	var force_x, force_y;
 
-func Move2Move()
-{
-	if (movement == 1)
+	var random = PV_Random(-10, 10);
+
+	if (move_style == 1)
 	{
-		SetYDir(-20);
-		SetXDir(-10 + Random(21));
+		force_y = -20;
+		force_x = PV_KeyFrames(0, 0, random, 300, random, 600, random, 1000, random);
 	}
-	if (movement == 2)
+	else if (move_style == 2)
 	{
-		SetYDir(-10 + Random(21));
-		SetXDir(-10 + Random(21));
+		force_x = force_y = PV_KeyFrames(0, 0, random, 300, random, 600, random, 1000, random);
 	}
-	
-	var _x, _y;
-	
-	_x = GetX();
-	_y = GetY();
-	
-	SetCon
-	(
-		75 * max_con / 100 + GetActTime() * 45 * max_con / (100 * max_time)
-	);
 
-	SetPosition(_x, _y);
+	if (!lifetime) lifetime = PV_Random(30, 35);
+	var particle = {Prototype = Particles_Thrust(),
+					Size = PV_Linear(75 * size / 100, 120 * size / 100),
+					ForceX = force_x,
+					ForceY = force_y};
 
-	if (effect == 1)
+	if (effect_style == 0)
 	{
-		SetClrModulation
-		(
-			RGBa
-			(
-				r1 + GetActTime() * (r2 - r1) / max_time,
-				g1 + GetActTime() * (g2 - g1) / max_time,
-				b1 + GetActTime() * (b2 - b1) / max_time,
-//				255 - (a1 + GetActTime() * (a2 - a1) / max_time)
-				a1 + GetActTime() * (a2 - a1) / max_time
-			)
-		);
+		particle.R = 255;
+		particle.G = 255;
+		particle.B = 255;
+		particle.Alpha = 128;
 	}
-	else if (effect == 3)
+	else if (effect_style == 1)
 	{
-		var i = Random(100);
+		particle.R = PV_Linear(r1, r2);
+		particle.G = PV_Linear(g1, g2);
+		particle.B = PV_Linear(b1, b2);
+		particle.Alpha = PV_Linear(a1, a2);
+	}
+	else if (effect_style >= 2)
+	{
+		particle.R = PV_Random(r1, r2, 100);
+		particle.G = PV_Random(g1, g2, 100);
+		particle.B = PV_Random(b1, b2, 100);
+		particle.Alpha = PV_Random(a1, a2, 100);
 		
-		SetClrModulation
-		(
-			RGBa
-			(
-					Min(r1, r2) + i * Abs(r2 - r1) / 100,
-					Min(g1, g2) + i * Abs(g2 - g1) / 100,
-					Min(b1, b2) + i * Abs(b2 - b1) / 100,
-//					255 - (Min(a1, a2) + i * Abs(a2 - a1) / 100)
-					Min(a1, a2) + i * Abs(a2 - a1) / 100
-			)
-		);
+		if (effect_style == 3)
+		{
+			particle.R = PV_KeyFrames(0, 0, particle.R, 300, particle.R, 600, particle.R, 1000, particle.R);
+			particle.G = PV_KeyFrames(0, 0, particle.G, 300, particle.G, 600, particle.G, 1000, particle.R);
+			particle.B = PV_KeyFrames(0, 0, particle.B, 300, particle.B, 600, particle.B, 1000, particle.R);
+			particle.Alpha = PV_KeyFrames(0, 0, particle.Alpha, 300, particle.Alpha, 600, particle.Alpha, 1000, particle.Alpha);
+		}		
 	}
-	
-	if (GetActTime() > max_time)
-		Remove();
+
+	CreateParticle("Smoke2", x, y, 0, 0, lifetime, particle);
 }
-
-func Remove()
-{
-	return RemoveObject();
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// properties
-
-local Name = "$Name$";
-local Description = "$Description$";
-local Collectible = 0;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// actions
-
-local ActMap = {
-Smoke = {
-	Prototype = Action,
-	Name = "Smoke",
-	Procedure = DFA_FLOAT,
-	Directions = 4,
-	Length = 4,
-	Delay = 0,
-	X = 0,
-	Y = 0,
-	Wdt = 64,
-	Hgt = 64,
-	NextAction = "Hold",
-},
-
-Smoke2 = {
-	Prototype = Action,
-	Name = "Smoke2",
-	Procedure = DFA_FLOAT,
-	Directions = 4,
-	Length = 4,
-	Delay = 0,
-	X = 0,
-	Y = 0,
-	Wdt = 64,
-	Hgt = 64,
-	NextAction = "Hold",
-},
-
-};
--- Arena.ocd/Effects.ocd/SmokeStack.ocd/Script.c
+++ Arena.ocd/Effects.ocd/SmokeStack.ocd/Script.c
@@ -1,29 +1,28 @@
 
-public func Launch(int time)
+public func Launch(int lifetime)
 {
 	local timer, end, change;
 
-	timer = time;
+	timer = lifetime;
 	end = timer / 2;
 
 	change = BoundBy(timer / 18, 1, timer);
 
-	SetAction("Travel");
-
 	local rgb1, rgb2;
 
-	rgb1 = 150 - Random(30);
-	rgb2 = 50 - Random(30);
+	rgb1 = 255 - Random(30);
+	rgb2 = 100 - Random(30);
+
+	SetAction("Travel");
 }
 
 private func Travel()
 {
 	timer = BoundBy(timer - change, end, timer);
 	
-	SmokeX(0, 0, timer, 1, 0, RGBa(rgb1, rgb1, rgb1, 190), RGBa(rgb2, rgb2, rgb2, 0), 1);
+	SmokeX(0, 0, timer, 2, RandomX(25, 40), RGBa(rgb1, rgb1, rgb1, 190), RGBa(rgb2, rgb2, rgb2, 0), 1);
 	
-	if (timer <= end)
-		RemoveObject();
+	if (timer <= end) RemoveObject();
 }
 
 private func ContactBottom()
@@ -43,18 +42,17 @@ local Collectible = 0;
 //
 // actions
 
-local ActMap = 
-{
-	Travel=
-	{
-		Prototype = Action,
-		Name = "Travel",
-		Procedure = DFA_NONE,
-		Directions = 1,
-		Length =  1,
-		Delay = 1,
-		FacetBase: 1,
-		NextAction = "Travel",
-		StartCall = "Travel"
-	}
+local ActMap = {
+Travel = {
+	Prototype = Action,
+	Name = "Travel",
+	Procedure = DFA_NONE,
+	Directions = 1,
+	Length = 1,
+	Delay = 1,
+	FacetBase = 1,
+	NextAction = "Travel",
+	StartCall = "Travel",
+},
+
 };
