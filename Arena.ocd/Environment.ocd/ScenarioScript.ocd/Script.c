9bc1440bfdd9cdfe4291948031ca0a6b70453819
Mark <markh4b@hotmail.com>
2015-07-13 06:56:45 +0200

    Relaunch: Basics
    Clonks relaunch after their death.

--- Arena.ocd/Environment.ocd/ScenarioScript.ocd/Script.c
+++ Arena.ocd/Environment.ocd/ScenarioScript.ocd/Script.c
@@ -13,3 +13,19 @@ func Initialize()
 	CreateObject(Environment_ConfigurationUA);
 	CreateObject(RoundTester);
 }
+
+
+protected func RelaunchPlayer(int player, int killer)
+{
+	var crew = CreateObject(Clonk, LandscapeWidth() / 2, 20, player);
+	crew->MakeCrewMember(player);
+	SetCursor(player, crew);
+
+	var relaunch_x = crew->GetX();
+	var relaunch_y = crew->GetY();
+
+	CreateObject(RelaunchContainerEx,
+	             relaunch_x,
+	             relaunch_y,
+	             crew->GetOwner())->StartRelaunch(crew);
+}
