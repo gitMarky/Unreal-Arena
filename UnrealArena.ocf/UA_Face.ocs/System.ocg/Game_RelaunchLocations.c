bef473152c97c5f39edb77b23a8ecc3288865461
Mark <markh4b@hotmail.com>
2016-10-28 06:47:30 +0200

    Fix relaunch location

--- UnrealArena.ocf/UA_Face.ocs/System.ocg/Game_RelaunchLocations.c
+++ UnrealArena.ocf/UA_Face.ocs/System.ocg/Game_RelaunchLocations.c
@@ -3,7 +3,7 @@
 func RelaunchLocations()
 {
 	return [{x = 125, y = 575, team = 1},
-	        {x = 120, y = 475, team = 2},
+	        {x = 120, y = 475, team = 1},
 	    	{x = LandscapeWidth() - 125, y = 575, team = 2},
 	    	{x = LandscapeWidth() - 120, y = 475, team = 2}];
 }
