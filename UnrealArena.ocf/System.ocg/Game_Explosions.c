5cfe5c898d6e06eeaad8faf4845723b086bc034b
Mark <markh4b@hotmail.com>
2016-11-08 07:03:55 +0100

    Explosions: Some stuff from UA

--- UnrealArena.ocf/System.ocg/Game_Explosions.c
+++ UnrealArena.ocf/System.ocg/Game_Explosions.c
@@ -38,7 +38,7 @@ global func BlastObjects(int x, int y, int level, object container, int cause_pl
 	// damage: if not specified this is the same as the explosion radius
 	if (damage_level == nil)
 		damage_level = level;
-
+	
 	// In a container?
 	if (container)
 	{
@@ -59,11 +59,10 @@ global func BlastObjectInContainer(object container, int cause_plr, int damage_l
 		container->BlastObject(damage_level, cause_plr);
 		if (!container)
 			return true; // Container could be removed in the meanwhile.
-		for (obj in FindObjects(Find_Container(container), Find_Layer(layer), Find_Exclude(prev_container)))
+		for (obj in FindObjects(Find_Container(container), Find_Layer(layer), Find_Exclude(prev_container))) 
 			if (obj)
 				obj->BlastObject(damage_level, cause_plr);
 	}
-
 }
 
 
@@ -73,26 +72,43 @@ global func BlastObjectNoContainer(int x, int y, int level, object container, in
 	
 	// Coordinates are always supplied globally, convert to local coordinates.
 	var l_x = x - GetX(), l_y = y - GetY();
-	
-	// Object is outside.
-	var at_rect = Find_AtRect(l_x - 5, l_y - 5, 10, 10);
-	// Damage objects at point of explosion.
-	for (var obj in FindObjects(at_rect, Find_NoContainer(), Find_Layer(layer), Find_Exclude(prev_container)))
-		if (obj) obj->BlastObject(damage_level, cause_plr);
-		
-	// Damage objects in radius.
-	for (var obj in FindObjects(Find_Distance(level, l_x, l_y), Find_Not(at_rect), Find_NoContainer(), Find_Layer(layer), Find_Exclude(prev_container)))
-		if (obj) obj->BlastObject(damage_level / 2, cause_plr);
 
-	
-	// Perform the shockwave at the location where the top level container previously was.
-	// This ensures reliable flint jumps for explosives that explode inside a crew member.
-	var off_x = 0, off_y = 0;
-	if (prev_container)
-	{	
-		var max_offset = 300;
-		off_x = BoundBy(-prev_container->GetXDir(100), -max_offset, max_offset);
-		off_y = BoundBy(-prev_container->GetYDir(100), -max_offset, max_offset);			
+	// Damage objects in radius.
+	for (var obj in FindObjects(Find_Distance(level, l_x, l_y), Find_NoContainer(), Find_Layer(layer), Find_Exclude(prev_container))) 
+	{
+		if (!obj) continue;
+
+		var help = CreateObject(Projectile_Explosion, RandomX(-5, 5), RandomX(-10, 10));
+		
+		var iAngle, iSpeed;
+		var explo = GetExplosionSpeed(this, obj, level);
+		obj->SetSpeed(explo[0], explo[1]);
+
+		iAngle = Angle(0, 0, explo[0], explo[1]);
+		iSpeed = Distance(0, 0, explo[0], explo[1]);
+
+		var percent = Sin(BoundBy(90 * (level - ObjectDistance(help, obj)) / level, 0, 90), 100);
+
+		help->DamageAmount(percent * damage_level / 100);
+		help->Velocity(iSpeed);
+		help->DamageType(DMG_Explosion);
+		help->HitScan();
+		// help->Launch(angle);
+		help->HitObject(obj, true);
+		
+		// somehow not removed
+		if (help) ScheduleCall(help, help.RemoveObject, 1, 0);
 	}
-	DoShockwave(x, y, level, cause_plr, layer, off_x, off_y);
+}
+
+global func GetExplosionSpeed(pCauser, pObj, iRadius)
+{
+	var x, y;
+
+	if (pCauser->GetX() < pObj->GetX()) x = pCauser->GetX() - pObj->GetX() + iRadius * 2;
+	if (pCauser->GetX() > pObj->GetX())	x = pCauser->GetX() - pObj->GetX() - iRadius * 2;
+	if (pCauser->GetY() < pObj->GetY())	y = pCauser->GetY() - pObj->GetY() + iRadius * 2;
+	if (pCauser->GetY() > pObj->GetY())	y = pCauser->GetY() - pObj->GetY() - iRadius * 2;
+	
+	return [x, y];
 }
