b7e9fa616cffc2839d071944e07b70b533abe9af
Mark <markh4b@hotmail.com>
2016-11-23 17:42:54 +0100

    StatusBar: Allow setting numbers

--- UnrealArena.ocf/Arena.ocd/HUD.ocd/Controllers.ocd/StatusBar.ocd/Script.c
+++ UnrealArena.ocf/Arena.ocd/HUD.ocd/Controllers.ocd/StatusBar.ocd/Script.c
@@ -6,8 +6,8 @@
 	@author Marky
 */
 
-local crew_gui_menu;
-local crew_gui_id;
+local gui_status_bar_menu;
+local gui_status_bar_id;
 local crew_bars;
 
 static const GUI_Controller_StatusBar_MarginLeft = 0;
@@ -37,16 +37,16 @@ func AssembleStatusBar()
 private func Construction()
 {
 	crew_bars = [];
-	crew_gui_menu = AssembleStatusBar();
-	crew_gui_id = GuiOpen(crew_gui_menu);
+	gui_status_bar_menu = AssembleStatusBar();
+	gui_status_bar_id = GuiOpen(gui_status_bar_menu);
 	
 	return _inherited(...);
 }
 
 private func Destruction()
 {
-	GuiClose(crew_gui_id);
-	crew_gui_id = nil;
+	GuiClose(gui_status_bar_id);
+	gui_status_bar_id = nil;
 
 	_inherited(...);
 }
@@ -207,7 +207,7 @@ private func ShowCrewBar(int bar)
 	var bottom = ToEmString(top + GUI_Controller_CrewBar_BarSize);
 	top = ToEmString(top);
 
-	crew_bars[bar].shown = GuiUpdate({ Player = GetOwner(), Top = top, Bottom = bottom }, crew_gui_id, crew_bars[bar].ID, this);
+	crew_bars[bar].shown = GuiUpdate({ Player = GetOwner(), Top = top, Bottom = bottom }, gui_status_bar_id, crew_bars[bar].ID, this);
 }
 
 // Sets the fill status of the bar. value is between 0 and 1000
@@ -222,7 +222,7 @@ private func SetCrewBarValue(int bar, int value, int text_val)
 	// Displaying the fill with Top = 100% creates an unwanted scrollbar
 	//if (value == 0) plr = NO_OWNER;
 
-	GuiUpdate({ fill = { Player = plr, Right = ToPercentString(value) }, text = { Text = bar_text } }, crew_gui_id, crew_bars[bar].ID, this);
+	GuiUpdate({ fill = { Player = plr, Right = ToPercentString(value) }, text = { Text = bar_text } }, gui_status_bar_id, crew_bars[bar].ID, this);
 }
 
 // Hides the bar that was saved in crew_bars[bar]
@@ -231,7 +231,7 @@ private func HideCrewBar(int bar)
 	if (!crew_bars[bar]) return;
 	if (!crew_bars[bar].shown) return;
 
-	GuiUpdate({ Player = NO_OWNER }, crew_gui_id, crew_bars[bar].ID, this);
+	GuiUpdate({ Player = NO_OWNER }, gui_status_bar_id, crew_bars[bar].ID, this);
 	crew_bars[bar].shown = false;
 
 	// Update position of all following bars
@@ -244,6 +244,21 @@ private func HideCrewBar(int bar)
 }
 
 /* Crew indices */
+
+public func SetNumbers(proplist info_tab, int value)
+{
+	var value100 = 0, value010 = 0, value001 = 0;
+
+	value100 = (value / 100) % 10;
+	value010 = (value / 10) % 10;
+	value001 =  value % 10;
+
+	info_tab.Element100.Digit.GraphicsName = Format("Number%d", value100);
+	info_tab.Element010.Digit.GraphicsName = Format("Number%d", value010);
+	info_tab.Element001.Digit.GraphicsName = Format("Number%d", value001);
+
+	GuiUpdate(gui_status_bar_menu, gui_status_bar_id);
+}
 
 
 
