dd6d67f7984f3e28c0d942d5faffaec2b95aa37c
Mark <markh4b@hotmail.com>
2017-05-11 06:53:54 +0200

    Correct inventory size

--- UnrealArena.ocf/System.ocg/Mod_ClonkShooterInventory.c
+++ UnrealArena.ocf/System.ocg/Mod_ClonkShooterInventory.c
@@ -7,15 +7,29 @@
 
 #appendto Clonk
 
+// callback from the extended relaunch container
+public func OnStartRelaunch()
+{
+	SetInventorySize();
+}
 
-public func OnRelaunched()
+// callback from the extended relaunch container
+public func OnFinishRelaunch()
+{
+	SetInventorySize();
+}
+
+
+public func SetInventorySize()
 {
 	if (IsInstaGibConfigured())
 	{
+		DebugLog("Set inventory size to 1");
 		SetMaxContentsCount(1);
 	}
 	else
 	{
+		DebugLog("Set inventory size to 10");
 		SetMaxContentsCount(10);
 	}
 }
--- UnrealArena.ocf/System.ocg/Mod_RelaunchContainer.c
+++ UnrealArena.ocf/System.ocg/Mod_RelaunchContainer.c
@@ -1,10 +1,20 @@
 #appendto RelaunchContainerEx
 
+
+public func OnInitializeCrew(object crew)
+{
+	//if (RoundManager()->IsRoundActive())
+	//{
+		crew->~OnStartRelaunch();
+	//}
+}
+
+
 public func OnRelaunchCrew(object crew)
 {
 	if (RoundManager()->IsRoundActive())
 	{
 		crew->Sound("SpawnPlayer");
-		crew->~OnRelaunch();
+		crew->~OnFinishRelaunch();
 	}
 }
