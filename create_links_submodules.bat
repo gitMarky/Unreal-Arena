adb9d4476ee36aee9d642c3e30c8c82fb87e158f
Mark <markh4b@hotmail.com>
2018-04-09 19:31:25 +0200

    Removed submodules

--- create_links_submodules.bat
+++
@@ -1,3 +0,0 @@
-mklink /D Links\LibraryArenaGames.ocd Library_ArenaGames\LibraryArenaGames.ocd
-mklink /D Links\LibraryShooter.ocd Library_Shooter\LibraryShooter.ocd
-mklink /D Links\LibraryCharacter.ocd Library_Character\LibraryCharacter.ocd
--- create_links_submodules.sh
+++
@@ -1,3 +0,0 @@
-ln -s /Library_ArenaGames/LibraryArenaGames.ocd /Links/LibraryArenaGames.ocd 
-ln -s /Library_Shooter/LibraryShooter.ocd /Links/LibraryShooter.ocd 
-ln -s /Library_Character/LibraryCharacter.ocd /Links/LibraryCharacter.ocd 
